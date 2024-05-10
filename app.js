42c397684f133129b9a264268fe1fac63740e7c0
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-05-10 16:20:17 -0400

    Prime Numbers Done

--- app.js
+++ app.js
@@ -2,22 +2,54 @@
 
 //Fizz Buzz
 
-//Loop from 1 - 100
-for (let i = 1; i <= 100; i++) {
-  //if the number is divisible by 3 and 5 "Fizz Buzz"
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
+function FizzBuzz(num) {
+  //Loop from 1 - 100
+  for (let i = 1; i <= num; i++) {
+    //if the number is divisible by 3 and 5 "Fizz Buzz"
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    }
+    //if the number is only divisible by 3 "Fizz"
+    else if (i % 3 === 0) {
+      console.log("Fizz");
+    }
+    //if the number is only divisible by 5 "Buzz"
+    else if (i % 5 === 0) {
+      console.log("Buzz");
+    }
+    //if the number is not divisible by either log the number
+    else {
+      console.log(i);
+    }
   }
-  //if the number is only divisible by 3 "Fizz"
-  else if (i % 3 === 0) {
-    console.log("Fizz");
+}
+
+// FizzBuzz(100);
+
+//Part Two
+//Prime Time
+
+function checkPrime(num) {
+  let flag = true;
+
+  for (i = 2; i < num; i++) {
+    if (num % i == 0) {
+      flag = false;
+      break;
+    }
   }
-  //if the number is only divisible by 5 "Buzz"
-  else if (i % 5 === 0) {
-    console.log("Buzz");
+
+  return flag;
+}
+
+let n = 9;
+
+while (true) {
+  let nextNum = n + 1;
+  if (checkPrime(nextNum)) {
+    console.log(nextNum);
+    break;
   }
-  //if the number is not divisible by either log the number
-  else {
-    console.log(i);
-  }
+
+  n++;
 }
